6985a626dfaf223c2a7fea65da77ac56327d2fa3
shawn <shawn@naturalprofessional.com>
2016-11-14 18:45:21 -0800

    Add link decoration js function which isn't working correctly

--- framework-bootstrap/js/application.js
+++ framework-bootstrap/js/application.js
@@ -1,3 +1,25 @@
+$(document).ready(function() {
+  // Assign target section link decoration
+
+
+// addLinkDecoration();
+
+});
+
+function addLinkDecoration() {
+  $("#navigation a").on('click', function() {
+    $(".current-page").removeClass("current-page");
+    var pathName = window.location.hash;
+    // console.log("path name " + pathName)
+
+    var selector = 'a[href$="' + pathName + '"]';
+    console.log("selector " + selector)
+    //
+    $(selector).addClass("current-page");
+  })
+}
+
+// Make the top nav sticky
 var mainNav = $(".main-nav");
 
 $(window).scroll(function() {
