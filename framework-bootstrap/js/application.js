cf39df7efa64cb25a38705f357df2cd14426fbf1
shawn <shawn@naturalprofessional.com>
2016-11-15 12:51:46 -0800

    Add resizing logo and fixed navbar

--- framework-bootstrap/js/application.js
+++ framework-bootstrap/js/application.js
@@ -1,31 +1,43 @@
-$(document).ready(function() {
-  // Assign target section link decoration
-
-
-// addLinkDecoration();
-
-});
-
-function addLinkDecoration() {
-  $("#navigation a").on('click', function() {
-    $(".current-page").removeClass("current-page");
-    var pathName = window.location.hash;
-    // console.log("path name " + pathName)
-
-    var selector = 'a[href$="' + pathName + '"]';
-    console.log("selector " + selector)
-    //
-    $(selector).addClass("current-page");
-  })
+function init() {
+  window.addEventListener('scroll', function(e){
+      var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+          shrinkOn = 300,
+          header = document.querySelector("header");
+      if (distanceY > shrinkOn) {
+          $(header).addClass("smaller")
+      } else {
+          if ($('header').hasClass("smaller")) {
+              $('header').removeClass("smaller")
+          }
+      }
+  });
 }
+window.onload = init();
+
+// $(document).ready(function() {
+// addLinkDecoration();
+// });
+
+// function addLinkDecoration() {
+//   $("#navigation a").on('click', function() {
+//     $(".current-page").removeClass("current-page");
+//     var pathName = window.location.hash;
+//     // console.log("path name " + pathName)
+//
+//     var selector = 'a[href$="' + pathName + '"]';
+//     console.log("selector " + selector)
+//     //
+//     $(selector).addClass("current-page");
+//   })
+// }
 
 // Make the top nav sticky
-var mainNav = $(".main-nav");
-
-$(window).scroll(function() {
-  if( $(this).scrollTop() > 200 ) {
-    mainNav.addClass("main-nav-scrolled");
-  } else {
-    mainNav.removeClass("main-nav-scrolled")
-  }
-});
+// var mainNav = $(".main-nav");
+//
+// $(window).scroll(function() {
+//   if( $(this).scrollTop() > 200 ) {
+//     mainNav.addClass("main-nav-scrolled");
+//   } else {
+//     mainNav.removeClass("main-nav-scrolled")
+//   }
+// });
